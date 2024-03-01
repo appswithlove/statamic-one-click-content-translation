d1d35bb390f243b845af21caca449f9c71f072de
Oleksii Brylin <brylin@moqod.com>
2024-03-01 16:10:39 +0100

    feat: Integration with Google Cloud Translate API? #14

--- src/Http/Controllers/TranslateMeController.php
+++ src/Http/Controllers/TranslateMeController.php
@@ -4,6 +4,7 @@ namespace Appswithlove\StatamicOneClickContentTranslation\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DeepL\Translator;
+use Google\Cloud\Translate\V3\TranslationServiceClient;
 
 class TranslateMeController
 {
@@ -17,13 +18,24 @@ class TranslateMeController
             $textStrings[] = $text['html'];
         }
 
-        try {
-            $translations = $this->translate($textStrings, $data['defaultLang'], $data['selectedLang']);
-        } catch (\DeepL\DeepLException $error) {
-            return response([
-                'code'      =>  400,
-                'message'   =>  $error->getMessage()
-            ], 400);
+        if (config('statamic-one-click-content-translation.service') === 'google') {
+            try {
+                $translations = $this->googleTranslate($textStrings, $data['defaultLang'], $data['selectedLang']);
+            } catch (\Error $error) {
+                return response([
+                    'code'      =>  400,
+                    'message'   =>  $error->getMessage()
+                ], 400);
+            }
+        } else {
+            try {
+                $translations = $this->deeplTranslate($textStrings, $data['defaultLang'], $data['selectedLang']);
+            } catch (\DeepL\DeepLException $error) {
+                return response([
+                    'code'      =>  400,
+                    'message'   =>  $error->getMessage()
+                ], 400);
+            }
         }
 
         $i = 0;
@@ -35,7 +47,7 @@ class TranslateMeController
         return $data;
     }
 
-    private function translate(array $texts, string $fromLang, string $toLang) {
+    private function deeplTranslate(array $texts, string $fromLang, string $toLang) {
         $authKey = config('statamic-one-click-content-translation.deepl_auth_key');
         if (!$authKey) {
             throw new \Exception('Empty Deepl Auth Key');
@@ -57,5 +69,43 @@ class TranslateMeController
         $translations = $translator->translateText($texts, $sourceLang, $toLang);
 
         return $translations;
+    }
+
+    private function googleTranslate(array $texts, string $fromLang, string $toLang)
+    {
+        $credentialsPath = base_path('statamic-one-click-content-translation.google_credetials');
+        if (!$credentialsPath) {
+            throw new \Exception('Empty Google Cloud credentials');
+        }
+
+        if (!file_exists($credentialsPath)) {
+            throw new \Exception("Can open credentials file: $credentialsPath");
+        }
+
+        $googleResourceId = base_path('statamic-one-click-content-translation.google_resource_id');
+
+        if (!$googleResourceId) {
+            throw new \Exception('Google Cloud resource ID empty');
+        }
+
+        $translationClient = new TranslationServiceClient([
+            'credentials' => $credentialsPath
+        ]);
+
+        $response = $translationClient->translateText(
+            $texts,
+            $toLang,
+            TranslationServiceClient::locationName($googleResourceId, 'global'),
+            [
+                'mimeType' => 'text/html',
+            ],
+        );
+
+        $translatedTexts = [];
+        foreach ($response->getTranslations() as $translation) {
+            $translatedTexts[] = (object) ['text' => $translation->getTranslatedText()];
+        }
+
+        return $translatedTexts;
     }
 }
