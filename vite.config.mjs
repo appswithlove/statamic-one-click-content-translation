c8f999293d9d10c27645b411e68a148df9d242b3
ViktorH <viktor.hurtovyi@gmail.com>
2025-05-28 10:45:01 +0200

    feat: vite instead of webpack

---
+++ vite.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  root: 'resources',
+  base: '/dist/',
+  build: {
+    outDir: path.resolve('dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        app: path.resolve('resources/js/index.js'),
+        style: path.resolve('resources/scss/main.scss'),
+      },
+      output: {
+        entryFileNames: 'js/statamic-one-click-content-translation.js',
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'css/statamic-one-click-content-translation.css';
+          }
+          return 'assets/[name].[hash][extname]';
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve('resources/js'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {},
+    },
+  },
+});
--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.sass('resources/scss/main.scss', 'css/statamic-one-click-content-translation.css');
-mix
-    .js('resources/js/index.js', 'js/statamic-one-click-content-translation.js')
-    .vue()
-    .setPublicPath('dist');
